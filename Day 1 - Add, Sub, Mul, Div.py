e0ce3784fa87e29e89ac9734b92c19f331576e32
gitkashyap <33050257+gitkashyap@users.noreply.github.com>
2024-08-02 18:51:19 +0530

    dailyPython - In this I Dev Kant Prajapati will be code 4 Python Problems on daily basis maintaining my consistency and discipline for 180Days starting with Day 1 on 2nd August.

---
+++ Day 1 - Add, Sub, Mul, Div.py
@@ -0,0 +1,66 @@
+'''Addition'''
+#M-1
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Addition : ", a+b)
+
+#M-2
+# #Defining Add function
+# def addition(a, b):
+#     return a+b
+# #Taking input
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Addition : ", addition(a, b))
+
+
+
+'''Subtraction'''
+#M-1
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Subtraction : ", a-b)
+
+#M-2
+#Defining Add function
+# def subtraction(a, b):
+#     return a-b
+# #Taking input
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Subtraction : ", subtraction(a, b))
+
+
+
+'''Multiplication'''
+# #M-1
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Multiplication : ", a*b)
+
+#M-2
+#Defining Add function
+# def mul(a, b):
+#     return a*b
+# #Taking input
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Multiplication : ", mul(a, b))
+
+
+
+
+'''Division'''
+# #M-1
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Division : ", a/b)
+
+#M-2
+#Defining Add function
+# def div(a, b):
+#     return a/b
+# #Taking input
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# print("Division : ", div(a, b))
