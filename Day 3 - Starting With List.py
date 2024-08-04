46682d0147bd96b148c67755e6a188b1e78ab31e
gitkashyap <33050257+gitkashyap@users.noreply.github.com>
2024-08-04 22:59:33 +0530

    Day 3 - 4th August - Starting with List today, 1. Python program to interchange first and last elements in a list                           2.  Python program to swap two elements in a list                           3. To find length of list                           4. Maximum and Minimum of two numbers in Python

---
+++ Day 3 - Starting With List.py
@@ -0,0 +1,89 @@
+'''Python program to interchange first and last elements in a list'''
+# creating a list
+# list1 = []
+#
+# #Taking input from user
+# n = int(input("Enter the number of elements in the list : "))
+# for i in range(n):
+#     element = input("Enter element " + str(i+1) + ": ")
+#
+#     #appending the elements to the list.
+#     list1.append(element)
+# print(f"Current List : ", list1)
+#
+# #interchanging the list's first and last element.
+# list1[0], list1[-1] = list1[-1], list1[0]
+# print("List after interchanging first and last elements : ", list1)
+
+
+
+
+
+
+'''Python program to swap two elements in a list'''
+# list1 = []
+#
+# #Taking input from user
+# n = int(input("Enter the number of elements in the list : "))
+# for i in range(n):
+#     element = input("Enter element " + str(i+1) + ": ")
+#
+#     #appending the elements to the list.
+#     list1.append(element)
+# print(f"Current List : ", list1)
+#
+# #which indexing is to be swapped
+# x = int(input("Enter the first index to be swapped : "))
+# y = int(input("Enter the second index to be swapped : "))
+#
+# #swapping elements
+# list1[x], list1[y] = list1[y], list1[x]
+# print("List after swapping elements at given indices : ", list1)
+
+
+
+
+
+
+''' To find Length of the list'''
+# list1 = []
+#
+# #Taking input from user
+# n = int(input("Enter the number of elements in the list : "))
+# for i in range(n):
+#     element = input("Enter element " + str(i+1) + ": ")
+#
+#     #appending the elements to the list.
+#     list1.append(element)
+# print(f"Length of the List : ", len(list1))
+
+
+
+
+
+'''Maximum of two numbers in Python
+Minimum of two numbers in Python'''
+#Defining function max
+# def max(a, b):
+#     if a>b: return a
+#     elif a==b: return b
+#     else: return b
+#
+# #Defining function min
+# def min(a, b):
+#     if a<b: return a
+#     elif a==b: return b
+#     else: return b
+#
+#
+# #Taking inputs from the user
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+#
+# #Printing maximum and minimum numbers
+# print("Maximum : ", max(a, b))
+# print("Minimum : ", min(a, b))
+
+
+
+
