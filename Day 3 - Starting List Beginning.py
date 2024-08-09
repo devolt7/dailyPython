830f04dc86060561436add4a7e1b9998aa755846
gitkashyap <33050257+gitkashyap@users.noreply.github.com>
2024-08-09 14:47:47 +0530

    Day 3  - August 4th - Starting List - interchanging elements, swapping, length of list, max and min.

---
+++ Day 3 - Starting List Beginning.py
@@ -0,0 +1,60 @@
+'''Python program to interchange first and last elements in a list'''
+
+# L1 = []
+# n = int(input("Enter Number of ELements in The List : "))
+# for i in range(n):
+#     element = input("Enter ELement " + str(i+1) + " : ")
+#     L1.append(element)
+# print("Current List : ", L1)
+# L1[0], L1[-1] = L1[-1], L1[0]
+# print("List after interchanging first and last elements : ", L1)
+
+
+
+
+'''Python program to swap two elements in a list'''
+# L1 = []
+# n = int(input("Enter Number of ELements in The List : "))
+# for i in range(n):
+#     element = input("Enter ELement " + str(i+1) + " : ")
+#     L1.append(element)
+# print("Current List : ", L1)
+# x = int(input("Enter first index of List to swap : "))
+# y = int(input("Enter second index of List to swap : "))
+# L1[x], L1[y] = L1[y], L1[x]
+# print("List after swapping elements at indices", x, "and", y, ":", L1)
+
+
+
+
+'''Python to find length of list'''
+# def length(x):
+#     return len(x)
+# L1 = []
+# n = int(input("Enter Number of ELements in The List : "))
+# for i in range(n):
+#     element = input("Enter ELement " + str(i+1) + " : ")
+#     L1.append(element)
+#
+# print("Current List : ", L1)
+# print("Length of List : ", length(L1))
+
+
+
+
+'''Maximum of two numbers in Python'''
+'''Maximum of two numbers in Python'''
+# def maxi(x):
+#     return max(x)
+#
+# def mini(x):
+#     return min(x)
+#
+# L1 = []
+# n = int(input("Enter Number of ELements in The List : "))
+# for i in range(n):
+#     element = int(input("Enter ELement " + str(i+1) + " : "))  # Convert input to integer
+#     L1.append(element)
+#
+# print("Current List : ", L1)
+# print(f"Maximum Element in this List is :", maxi(L1), "\nMinimum Element in this List is : ", mini(L1))
