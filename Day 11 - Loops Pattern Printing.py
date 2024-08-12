31d80c6c88f4840ce6b4c51406f3b5bee2f540ed
gitkashyap <33050257+gitkashyap@users.noreply.github.com>
2024-08-12 22:16:51 +0530

    Day 11  - August 12th - Loop Pattern Printing Practice

---
+++ Day 11 - Loops Pattern Printing.py
@@ -0,0 +1,42 @@
+'''Increasing Triangle in Python using Loop'''
+# n = int(input("Enter Height Of Triangle : "))
+# for i in range (n):
+#     for j in range(i+1):
+#         print("*", end=' ')
+#     print(" ")
+
+
+
+
+'''Decreasing Right Triangle in Python using Loop'''
+# n = int(input("Enter Height Of Triangle : "))
+# for i in range (n):
+#     for j in range(0, n-i):
+#         print("*", end=' ')
+#     print(" ")
+
+
+
+
+'''Increasing Right sided Triangle in Python using loop'''
+# #Using Nested Loop
+# n = int(input("Enter Height Of Triangle : "))
+# for i in range (n):
+#     for j in range(i, n):
+#         print(" ", end=' ')
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     print(" ")
+
+
+
+
+'''Decreasing Right sided Triangle in Python using loop'''
+#Using Nested Loop
+# n = int(input("Enter Height Of Triangle : "))
+# for i in range (n):
+#     for j in range(i + 1):
+#         print(" ", end=' ')
+#     for j in range(i, n):
+#         print("*", end=" ")
+#     print(" ")
